e7eff66312dcfe2ac1c19b657244fe73347720b9
Aditya Kendre <kendreaditya@gmail.com>
2021-03-18 23:57:17 -0400

    added @material-ui/core and created filtration dropdown based on material-ui and created better fake data

--- pages/components/SearchFilter.js
+++ pages/components/SearchFilter.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import Search from './Search';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
 
 const filterElements = (prop, elements) => {
-    // Add custom filters for filters: Data, Topic, Poster (Student or Platform), Location (In-person or Online)
+    // Add custom filters for filters: Date, Topic, Poster (Student or Platform), Location (In-person or Online)
     // Also ascending or decending
     elements.sort((a, b) => {
+        if (["date"].includes(prop)) {
+            return (parseInt(a[prop]) > parseInt(b[prop])) ? 1 : ((parseInt(a[prop]) < parseInt(b[prop])) ? -1 : 0);
+        } else if (["topic", "poster", "location"].includes(prop)) {
+            return a[prop].localeCompare(b[prop]);
+        }
         return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
     });
     return elements;
@@ -12,15 +22,14 @@ const filterElements = (prop, elements) => {
 
 const SearchFilter = ({ filters, courses }) => {
     const filterOptions = filters.map((filter, idx) => (
-        <option value={idx}>
+        <MenuItem value={idx}>
             {filter}
-        </option>
+        </MenuItem >
     ));
 
     const [elements, setElementOrder] = useState(courses)
-    const [filter, setFilter] = useState('Date')
+    const [filter, setFilter] = useState(filters[0])
     useEffect(() => {
-        console.log(elements)
         let orderedElements = filterElements(filter, elements)
         setElementOrder(orderedElements);
         console.log(elements)
@@ -28,9 +37,12 @@ const SearchFilter = ({ filters, courses }) => {
 
 
     return (<>
-        <select className="ui dropdown" onChange={(e) => (setFilter(e.target.value))}>
-            {filterOptions}
-        </select>
+        <FormControl>
+            <InputLabel>Filter</InputLabel>
+            <Select onChange={(e) => (setFilter(filters[e.target.value]))}>
+                {filterOptions}
+            </Select>
+        </FormControl>
     </>);
 };
 
