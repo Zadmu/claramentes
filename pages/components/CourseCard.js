7bb14d23310fa9867af2feff94494828748e69a2
notunavailable <77205554+notunavailable@users.noreply.github.com>
2021-10-26 14:32:37 -0700

    added course card totopics page

---
+++ pages/components/CourseCard.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import courseCSS from '../../css/courseCard.module.css';
+import courses from '../../data/courses2.json';
+
+const CourseCard = () => {
+
+    const renderCourses = courses.map((course, i) => {
+        return(
+            <div key = {i} style={{
+                height: "259px",
+                width: "340px",
+                marginLeft: "50px",
+                position: "relative"
+            }}>
+                <div className = {courseCSS.taughtBy}>
+                    <h3 className = {courseCSS.owner}>Taught by:<br/>{course.admins}</h3>
+                </div>
+                <div className = {courseCSS.qualificationContainer}>
+                    <h3 className = {courseCSS.qualification}>Qualifications:<br/>{course.qualification}</h3>
+                </div>
+                <img src = {course.picture} className = {courseCSS.image}/>
+                <div style={{
+                    height: "240px",
+                    background: "#F4EED9",
+                    width: "340px",
+                    marginTop: "19px",
+                    borderRadius: "20px",
+                    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                }}>
+                    <div style = {{
+                        background: "rgba(0, 0, 0, 0.1)",
+                        borderRadius: "10px",
+                        left: "13.82%",
+                        top: "74.13%",
+                        width: "246px",
+                        height: "56px",
+                        position: "absolute"
+                    }}>
+                        <h2 className = {courseCSS.name}>{course.name}</h2>
+                    </div>
+                </div>
+            </div>
+        )
+    })
+
+
+    
+    return(
+        <div className = {courseCSS.row}>
+            {renderCourses}
+        </div>
+    );
+
+}
+
+export default CourseCard;
--- pages/components/TopicsNav.js
+++ pages/components/TopicsNav.js
@@ -1,9 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import topicsCSS from '../../css/topicsNav.module.css';
 import TopicsRow from './TopicsRow';
 import ExtendTopicsNav from './ExtendTopicsNav';
+import CourseCard from './CourseCard';
+import courseCSS from '../../css/courseCard.module.css';
+
 
     const TopicsNav = ({setTopicRow, topicsList, setSelectedTopics, selectedTopics, topicRow, currentSelectedTopic, setCurrentSelectedTopic}) => {
+
 
         if(topicRow == 3){
             return (
@@ -13,14 +17,21 @@ import ExtendTopicsNav from './ExtendTopicsNav';
                     <TopicsRow setTopicRow = {setTopicRow} css = {`${topicsCSS.row} ${topicsCSS.thirdRowPosition}`} currentRenderedRow = {3} setSelectedTopics = {setSelectedTopics} selectedTopics = {selectedTopics} topicsList = {topicsList} currentSelectedTopic = {currentSelectedTopic} setCurrentSelectedTopic = {setCurrentSelectedTopic}/>
                     <div className = {topicsCSS.bigTopicBoxThreeRow}>
                         <ExtendTopicsNav hasChildren = {selectedTopics[currentSelectedTopic].hasChildren} setTopicRow = {setTopicRow} topicRow = {topicRow} selectedTopicRow = {selectedTopics[currentSelectedTopic].row}/>
-                        <h1 className = {topicsCSS.topicName}>{selectedTopics[currentSelectedTopic].name}</h1>
+                        <div className = {topicsCSS.topicNameThreeRow}>
+                            <h1 className = {topicsCSS.topicName}>{selectedTopics[currentSelectedTopic].name}</h1>
+                        </div>
                         <div className = {topicsCSS.descriptionBox}>
                             <h2 className = {topicsCSS.descriptionText}>{selectedTopics[currentSelectedTopic].Description}</h2>
+                        </div>
+                        <div>
+                            <h2 className = {courseCSS.popularCourses}>Popular Courses</h2>
+                            <CourseCard />
                         </div>
                     </div>
                 </div>
             );
         }
+
         else if(topicRow == 2){
             return (
                 <div>
@@ -28,23 +39,36 @@ import ExtendTopicsNav from './ExtendTopicsNav';
                     <TopicsRow setTopicRow = {setTopicRow} css = {`${topicsCSS.row} ${topicsCSS.secondRowPosition}`} currentRenderedRow = {2} setSelectedTopics = {setSelectedTopics} selectedTopics = {selectedTopics} topicsList = {topicsList} currentSelectedTopic = {currentSelectedTopic} setCurrentSelectedTopic = {setCurrentSelectedTopic}/>
                     <div className = {topicsCSS.bigTopicBoxTwoRow}>
                         <ExtendTopicsNav hasChildren = {selectedTopics[currentSelectedTopic].hasChildren} setTopicRow = {setTopicRow} topicRow = {topicRow} selectedTopicRow = {selectedTopics[currentSelectedTopic].row}/>
-                        <h1 className = {topicsCSS.topicName}>{selectedTopics[currentSelectedTopic].name}</h1>
+                        <div className = {topicsCSS.topicNameTwoRow}>
+                            <h1 className = {topicsCSS.topicName}>{selectedTopics[currentSelectedTopic].name}</h1>
+                        </div>
                         <div className = {topicsCSS.descriptionBox}>
                             <h2 className = {topicsCSS.descriptionText}>{selectedTopics[currentSelectedTopic].Description}</h2>
+                        </div>
+                        <div>
+                            <h2 className = {courseCSS.popularCourses}>Popular Courses</h2>
+                            <CourseCard/>
                         </div>
                     </div>
                 </div>
             );
         }
+        
         else{
             return (
                 <div>
                     <TopicsRow setTopicRow = {setTopicRow} css = {`${topicsCSS.row} ${topicsCSS.firstRowPosition}`} currentRenderedRow = {1} setSelectedTopics = {setSelectedTopics} selectedTopics = {selectedTopics} topicsList = {topicsList} currentSelectedTopic = {currentSelectedTopic} setCurrentSelectedTopic = {setCurrentSelectedTopic}/>
                     <div className = {topicsCSS.bigTopicBoxOneRow}>
                         <ExtendTopicsNav hasChildren = {selectedTopics[currentSelectedTopic].hasChildren} setTopicRow = {setTopicRow} topicRow = {topicRow} selectedTopicRow = {selectedTopics[currentSelectedTopic].row}/>
-                        <h1 className = {topicsCSS.topicName}>{selectedTopics[currentSelectedTopic].name}</h1>    
+                        <div className = {topicsCSS.topicNameOneRow}>
+                            <h1 className = {topicsCSS.topicName}>{selectedTopics[currentSelectedTopic].name}</h1>    
+                        </div>
                         <div className = {topicsCSS.descriptionBox}>
                             <h2 className = {topicsCSS.descriptionText}>{selectedTopics[currentSelectedTopic].Description}</h2>
+                        </div>
+                        <div>
+                            <h2 className = {courseCSS.popularCourses}>Popular Courses:</h2>
+                            <CourseCard/>
                         </div>        
                     </div>
                 </div>
